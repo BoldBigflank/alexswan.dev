98f45741db5ed34f56ce7519cb52c29e9d3095e7
Alex Swan <alex@jackboxgames.com>
2022-12-12 14:37:35 -0600

    holiday 2022 wip

---
+++ pages/holiday2022.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export const Portfolio = (): JSX.Element => {
+  return (
+    <Layout
+      customMeta={{
+        title: 'Swan Family 2022 Letter',
+      }}
+    >
+      <h1>Swan Family 2022 Letter</h1>
+      <div className="title">WIP</div>
+    </Layout>
+  );
+};
+
+export default Portfolio;
